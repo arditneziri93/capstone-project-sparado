92dc1f0665cc1b93927beb8847c43ed932da8c80
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-06-06 21:29:11 +0200

    Create 404 page
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { H1, BB } from "../components/typography.js";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function Custom404() {
+  return (
+    <Container>
+      <H1>404</H1>
+
+      <BB>This page could not be found.</BB>
+    </Container>
+  );
+}
