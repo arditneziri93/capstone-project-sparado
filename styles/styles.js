c3623a35351bd50f5695a1fc3f7b771c949559d9
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-06-06 21:30:48 +0200

    Remove anchor styling
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- styles/styles.js
+++ styles/styles.js
@@ -14,4 +14,20 @@ export default createGlobalStyle`
     background-color: ${({ theme }) => theme.surface.neutralAlt};
     color: ${({ theme }) => theme.text.neutral};
   }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  a:hover{
+    text-decoration: none;
+    color: inherit;
+  }
+
+
+  a:active{
+    text-decoration: none;
+    color: inherit;
+  }
 `;
