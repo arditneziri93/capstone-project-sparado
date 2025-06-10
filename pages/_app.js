424201fa4643064e8bb3419f1d5b4732d6405108
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-06-10 11:26:18 +0200

    Create app layout
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/styles";
 import light from "../styles/themes/light";
+import dark from "../styles/themes/dark";
+import AppLayout from "../components/layout/app_layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={light}>
-        <GlobalStyle />
-        <Component {...pageProps} />
+        <AppLayout>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </AppLayout>
       </ThemeProvider>
     </>
   );
