d09b26ae68d4505b969cc41ad54a2376f8c5891b
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-06-11 02:36:22 +0200

    Add new icons
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- components/icons/_icon_type.js
+++ components/icons/_icon_type.js
@@ -1,5 +1,13 @@
 import Home from "@/pages";
-import { HomeTrendUp, Setting2, Tag2, Coin1, Home2 } from "iconsax-reactjs";
+import {
+  HomeTrendUp,
+  Setting2,
+  Tag2,
+  Coin1,
+  Home2,
+  Sun1,
+  Moon,
+} from "iconsax-reactjs";
 
 export const IconType = {
   HOME: (props) => <Home2 {...props} />,
@@ -7,4 +15,6 @@ export const IconType = {
   TRANSACTIONS: (props) => <Coin1 {...props} />,
   CATEGORIES: (props) => <Tag2 {...props} />,
   SETTINGS: (props) => <Setting2 {...props} />,
+  LIGHTMODE: (props) => <Sun1 {...props} />,
+  DARKMODE: (props) => <Moon {...props} />,
 };
