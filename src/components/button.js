304fe95b6d8810167c331c0da28e912be52209dd
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-06-13 23:39:26 +0200

    Refactor Button Component
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { css, useTheme } from "styled-components";
+import styled from "styled-components";
 import { LM } from "./typography";
 
 export const ButtonType = {
@@ -8,79 +8,175 @@ export const ButtonType = {
   GHOST: "ghost",
 };
 
+export const ButtonState = {
+  DEFAULT: "default",
+  SUCCESS: "success",
+  DANGER: "danger",
+  WARNING: "warning",
+  INFO: "info",
+  DISABLED: "disabled",
+};
+
 const ButtonStyled = styled.button`
   padding: ${({ theme }) => `${theme.size.ml} ${theme.size.xl}`};
-  border-radius: ${({ theme }) => theme.size.ml};
+  border-radius: ${({ theme }) => theme.size.m};
   font-weight: bold;
-  border: none;
-  transition: all 0.2s ease;
   display: inline-flex;
   align-items: center;
   justify-content: center;
   text-transform: uppercase;
-  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+  border: none;
+  &:hover {
+    cursor: pointer;
+  }
 
   &.filled {
-    ${({ theme, disabled }) => css`
-      background-color: ${disabled
-        ? theme.surface.disabled
-        : theme.surface.accent};
-      &:hover {
-        background-color: ${!disabled && theme.surface.accentHover};
-      }
-    `}
+    color: ${({ theme }) => theme.text.onDark};
+  }
+
+  &.filled.default {
+    background-color: ${({ theme }) => theme.surface.accent};
+    &:hover {
+      background-color: ${({ theme }) => theme.surface.accentHover};
+    }
+  }
+
+  &.filled.success {
+    background-color: ${({ theme }) => theme.surface.success};
+    &:hover {
+      background-color: ${({ theme }) => theme.surface.successHover};
+    }
+  }
+
+  &.filled.danger {
+    background-color: ${({ theme }) => theme.surface.danger};
+    &:hover {
+      background-color: ${({ theme }) => theme.surface.dangerHover};
+    }
+  }
+
+  &.filled.warning {
+    background-color: ${({ theme }) => theme.surface.warning};
+    &:hover {
+      background-color: ${({ theme }) => theme.surface.warningHover};
+    }
+  }
+
+  &.filled.info {
+    background-color: ${({ theme }) => theme.surface.info};
+    &:hover {
+      background-color: ${({ theme }) => theme.surface.infoHover};
+    }
+  }
+
+  &.filled.disabled {
+    background-color: ${({ theme }) => theme.surface.disabled};
+    color: ${({ theme }) => theme.text.onDark};
   }
 
   &.outline {
-    ${({ theme, disabled }) => css`
+    background-color: transparent;
+    border: ${({ theme }) => theme.border.neutral} solid;
+    padding: ${({ theme }) => {
+      const ml = parseInt(theme.size.ml);
+      const xl = parseInt(theme.size.xl);
+      const border = parseInt(theme.border.size);
+      return `${ml - border}px ${xl - border}px`;
+    }};
+    &:hover {
+      background-color: ${({ theme }) => theme.surface.neutralHover};
+    }
+  }
+
+  &.outline.default {
+    color: ${({ theme }) => theme.text.accent};
+    border-color: ${({ theme }) => theme.border.accent};
+  }
+
+  &.outline.success {
+    color: ${({ theme }) => theme.text.success};
+    border-color: ${({ theme }) => theme.border.success};
+  }
+
+  &.outline.danger {
+    color: ${({ theme }) => theme.text.danger};
+    border-color: ${({ theme }) => theme.border.danger};
+  }
+
+  &.outline.warning {
+    color: ${({ theme }) => theme.text.warning};
+    border-color: ${({ theme }) => theme.border.warning};
+  }
+
+  &.outline.info {
+    color: ${({ theme }) => theme.text.info};
+    border-color: ${({ theme }) => theme.border.info};
+  }
+
+  &.outline.disabled {
+    color: ${({ theme }) => theme.text.disabled};
+    border-color: ${({ theme }) => theme.surface.disabled};
+    &:hover {
       background-color: transparent;
-      border: 2px solid ${disabled ? theme.surface.disabled : theme.text.accent};
-      &:hover {
-        background-color: ${!disabled && theme.surface.specialHover};
-      }
-    `}
+    }
   }
 
   &.ghost {
-    ${({ theme, disabled }) => css`
+    background-color: transparent;
+    &:hover {
+      background-color: ${({ theme }) => theme.surface.neutralHover};
+    }
+  }
+
+  &.ghost.default {
+    color: ${({ theme }) => theme.text.neutral};
+  }
+
+  &.ghost.success {
+    color: ${({ theme }) => theme.text.success};
+  }
+
+  &.ghost.danger {
+    color: ${({ theme }) => theme.text.danger};
+  }
+
+  &.ghost.warning {
+    color: ${({ theme }) => theme.text.warning};
+  }
+
+  &.ghost.info {
+    color: ${({ theme }) => theme.text.info};
+  }
+
+  &.ghost.disabled {
+    color: ${({ theme }) => theme.text.disabled};
+    &:hover {
       background-color: transparent;
-      &:hover {
-        background-color: ${!disabled && theme.surface.neutralHover};
-      }
-    `}
+    }
+  }
+
+  &.disabled:hover {
+    cursor: not-allowed;
   }
 `;
 
 export function Button({
   label,
   type = ButtonType.FILLED,
-  disabled = false,
+  state = ButtonState.DEFAULT,
   onClick,
   ...props
 }) {
-  const theme = useTheme();
-
-  const resolveColor = () => {
-    switch (type) {
-      case ButtonType.FILLED:
-        return theme.text.onDark;
-      case ButtonType.OUTLINE:
-        return theme.text.accent;
-      case ButtonType.GHOST:
-      default:
-        return theme.text.neutral;
-    }
-  };
+  const finalClass = `${type} ${state}`;
 
   return (
     <ButtonStyled
       type="button"
-      className={type}
-      disabled={disabled}
-      onClick={onClick}
+      className={finalClass}
+      onClick={state === ButtonState.DISABLED ? undefined : onClick}
       {...props}
     >
-      <LM color={resolveColor()}>{label}</LM>
+      <LM color="inherit">{label}</LM>
     </ButtonStyled>
   );
 }
