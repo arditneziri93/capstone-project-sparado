92a4728dd3d6569415ae4cddb6a389630482445d
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-06-15 21:46:39 +0200

    Remove console.log
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- src/utils/get_color_by_sign.js
+++ src/utils/get_color_by_sign.js
@@ -1,6 +1,5 @@
 export default function getColorBySign(value, theme) {
   const parsedValue = parseFloat(value);
-  console.log(parsedValue + " " + typeof parsedValue);
   if (isNaN(parsedValue)) return theme.text.neutral;
   if (parsedValue > 0) return theme.text.success;
   if (parsedValue < 0) return theme.text.danger;
