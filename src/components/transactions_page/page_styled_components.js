b4d720fc93f4f186006e6323c544a2ccf69cb1c6
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-06-16 12:20:28 +0200

    Refactor styled components
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

---
+++ src/components/transactions_page/page_styled_components.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+const TableHeader = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.surface.neutral};
+  border-radius: ${({ theme }) => theme.size.m};
+  overflow: hidden;
+  position: sticky;
+  top: -75px;
+  z-index: 10;
+  box-shadow: 5px 0px 20px ${({ theme }) => theme.surface.neutralAlt};
+`;
+
+const TableNavigationWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: stretch;
+  padding: ${({ theme }) => theme.size.m + " " + theme.size.l};
+  gap: ${({ theme }) => theme.size.l};
+  align-items: center;
+`;
+
+const TableContentWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: ${({ theme }) => theme.size.m + " " + theme.size.none};
+`;
+
+const TransactionsGroupWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  padding: ${({ theme }) => theme.size.m + " " + theme.size.none};
+  gap: ${({ theme }) => theme.size.s};
+`;
+
+const TransactionGroupDateRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${({ theme }) => theme.size.m + " " + theme.size.l};
+`;
+
+const PlaceholderWrapper = styled.div`
+  width: 100%;
+  padding: ${({ theme }) => theme.size.xl2};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: ${({ theme }) => theme.size.xl3};
+`;
+
+export {
+  TableHeader,
+  TableNavigationWrapper,
+  TableContentWrapper,
+  TransactionsGroupWrapper,
+  TransactionGroupDateRow,
+  PlaceholderWrapper,
+};
--- src/pages/transactions/index.js
+++ src/pages/transactions/index.js
@@ -11,70 +11,18 @@ import { useTransactionStore } from "@/src/stores/transactions_store";
 import { LM } from "@/src/components/shared/typography";
 import { TableFooter } from "@/src/components/transactions_page/table_footer";
 import { useTheme } from "styled-components";
+import {
+  TableHeader,
+  TableNavigationWrapper,
+  TableContentWrapper,
+  TransactionsGroupWrapper,
+  TransactionGroupDateRow,
+  PlaceholderWrapper,
+} from "@/src/components/transactions_page/page_styled_components";
 
 export default function TransactionsPage() {
   const { groupedTransactions } = useTransactionStore();
   const theme = useTheme();
-
-  const TableHeader = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: ${({ theme }) => theme.surface.neutral};
-    border-radius: ${({ theme }) => theme.size.m};
-    overflow: hidden;
-    position: sticky;
-    top: -75px;
-    z-index: 10;
-    box-shadow: 5px 0px 20px ${({ theme }) => theme.surface.neutralAlt};
-  `;
-
-  const TableNavigationWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: stretch;
-    padding: ${({ theme }) => theme.size.m + " " + theme.size.l};
-    gap: ${({ theme }) => theme.size.l};
-    align-items: center;
-  `;
-
-  const TableContentWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: ${({ theme }) => theme.size.m + " " + theme.size.none};
-  `;
-
-  const TransactionsGroupWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: start;
-    align-items: start;
-    padding: ${({ theme }) => theme.size.m + " " + theme.size.none};
-    gap: ${({ theme }) => theme.size.s};
-  `;
-
-  const TransactionGroupDateRow = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: ${({ theme }) => theme.size.m + " " + theme.size.l};
-  `;
-
-  const PlaceholderWrapper = styled.div`
-    width: 100%;
-    padding: ${({ theme }) => theme.size.xl2};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: ${({ theme }) => theme.size.xl3};
-  `;
 
   function goToCurrentMonth() {
     alert("Current Month");
