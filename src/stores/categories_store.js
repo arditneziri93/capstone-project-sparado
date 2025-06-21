7f893aa9ba4c3513773daba933f348e37934baaf
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-06-21 03:00:27 +0200

    Add feature
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- src/stores/categories_store.js
+++ src/stores/categories_store.js
@@ -3,6 +3,18 @@ import { useCategories } from "@/src/services/categories_service";
 export const useCategoryStore = () => {
   const [categories, setCategories, deleteCategoryService] = useCategories();
 
+  async function addCategory(category) {
+    setCategories((prev) => [...prev, category]);
+    return true;
+  }
+
+  async function updateCategory(category) {
+    setCategories((prev) =>
+      prev.map((c) => (c.id === category.id ? category : c))
+    );
+    return true;
+  }
+
   async function deleteCategory(id) {
     return await deleteCategoryService(id);
   }
@@ -10,6 +22,8 @@ export const useCategoryStore = () => {
   return {
     categories,
     setCategories,
+    addCategory,
+    updateCategory,
     deleteCategory,
   };
 };
